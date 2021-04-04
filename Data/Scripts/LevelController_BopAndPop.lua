ce38d460905bed18f1561b3373e059c9067aa521
GrandMoffDave <dbernat@me.com>
2021-04-04 01:04:17 -0700

    bug fixes bnp

--- Data/Scripts/LevelController_BopAndPop.lua
+++ Data/Scripts/LevelController_BopAndPop.lua
@@ -229,7 +229,9 @@ function UnloadTutorial()
 		-- 		liveBalloon:Destroy()
 		-- 	end
 		-- end
-		object:Destroy()
+		if object:IsValid() then
+			object:Destroy()
+		end
 	end
 
 	intakeCycleColors = {}
@@ -294,6 +296,22 @@ end
 function LevelPowerDown()
 	UnloadInterior()
 	script:SetNetworkedCustomProperty("levelStatus", 0)
+	for _, object in ipairs(propTutorialContainer:GetChildren()) do
+		-- print("unloading tutorial object " .. object.id)
+
+		-- if object.name == "bnp_balloon" then
+		-- 	local	liveBalloon = object.context.propPhysics or object.context.propEquipment
+			
+		-- 	print("balloon " .. object.id .. " has live balloon " .. liveBalloon.id)
+		-- 	if liveBalloon ~= nil and liveBalloon:IsValid() then
+		-- 		print("destroying it")
+		-- 		liveBalloon:Destroy()
+		-- 	end
+		-- end
+		if object:IsValid() then
+			object:Destroy()
+		end
+	end
 end
 
 function LevelBegin()
