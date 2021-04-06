c23064851ee9b3c52cd834fe0ea65b3313d3602b
GrandMoffDave <dbernat@me.com>
2021-04-05 22:20:09 -0700

    reset bnp score on powerup

--- Data/Scripts/LevelController_BopAndPop.lua
+++ Data/Scripts/LevelController_BopAndPop.lua
@@ -285,6 +285,8 @@ function LevelPowerUp()
 	LoadInterior()
 	LoadTutorial()
 
+	teamScore = 0
+	teamFailures = 0
 	script:SetNetworkedCustomProperty("currentScore", teamScore)
 	script:SetNetworkedCustomProperty("strikeCount", teamFailures)
 	script:SetNetworkedCustomProperty("levelStatus", 1)
@@ -303,22 +305,6 @@ function LevelPowerDown()
 
 	UnloadInterior()
 	script:SetNetworkedCustomProperty("levelStatus", 0)
-	-- for _, object in ipairs(propTutorialContainer:GetChildren()) do
-	-- 	-- print("unloading tutorial object " .. object.id)
-
-	-- 	-- if object.name == "bnp_balloon" then
-	-- 	-- 	local	liveBalloon = object.context.propPhysics or object.context.propEquipment
-			
-	-- 	-- 	print("balloon " .. object.id .. " has live balloon " .. liveBalloon.id)
-	-- 	-- 	if liveBalloon ~= nil and liveBalloon:IsValid() then
-	-- 	-- 		print("destroying it")
-	-- 	-- 		liveBalloon:Destroy()
-	-- 	-- 	end
-	-- 	-- end
-	-- 	if object:IsValid() then
-	-- 		object:Destroy()
-	-- 	end
-	-- end
 	
 	for _, balloon in pairs(propLiveBalloons) do
 		if balloon:IsValid() then
